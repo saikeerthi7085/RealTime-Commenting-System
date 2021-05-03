33fe48ad988050dcb64f4aa9ccaa8e106186c5b3
saikeerthi7085 <saikeerthimaddineni@gmail.com>
2021-05-03 15:16:12 +0100

    Uploading Code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+﻿using PusherServer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Real_Time_Commenting.Models;
+
+namespace Real_Time_Commenting.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+
+            return View(db.BlogPost.AsQueryable());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(BlogPost post)
+        {
+            db.BlogPost.Add(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int? id)
+        {
+            return View(db.BlogPost.Find(id));
+        }
+
+        public ActionResult Comments(int? id)
+        {
+            var comments = db.Comment.Where(x => x.BlogPostID == id).ToArray();
+            return Json(comments, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Comment(Comment data)
+        {
+            db.Comment.Add(data);
+            db.SaveChanges();
+            var options = new PusherOptions();
+            options.Cluster = "eu";
+            var pusher = new Pusher("1197819", "14c622d6af591c904357", "b0cd280773ee163327d4", options);
+            ITriggerResult result = await pusher.TriggerAsync("asp_channel", "asp_event", data);
+            return Content("ok");
+        }
+
+        // GET: BlogPosts/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = await db.BlogPost.FindAsync(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "BlogPostID,Title,Body")] BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blogPost).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = await db.BlogPost.FindAsync(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            BlogPost blogPost = await db.BlogPost.FindAsync(id);
+            db.BlogPost.Remove(blogPost);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
